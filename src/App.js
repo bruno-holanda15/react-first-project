5ad99a6d6dc1927ea57377e482e6e5e2563d2559
Bruno Holanda <br.holanda15@gmail.com>
2022-10-01 16:35:14 -0300

    Using arrow functions and states with class component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 import { Component } from 'react';
 
 //component de classe
-class App extends Component
-{
+class App extends Component {
+
+  state = {
+    name: 'Brunin Holandis',
+    counter: 0
+  };
+
+  handlePClick= () => {
+    this.setState({name: 'Peter Miranha'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    this.setState({counter: this.state.counter + 1})
+  }
+
   render () {
+    const { name, counter } = this.state;
     return (
           <div className="App">
             <header className="App-header">
@@ -13,7 +28,11 @@ class App extends Component
               <p>
                 Edit <code>src/App.js</code> and save to reload.
               </p>
+              <p onClick={this.handlePClick}>
+                Meu nome {name} - Counter {counter}
+              </p>
               <a
+                onClick={this.handleAClick}
                 className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
